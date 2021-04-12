587cb377f7a7187c0e82ddd394ad6f2121a8d02a
Alex93pa <Ale.pug2@gmail.com>
2021-04-12 16:02:24 +0200

    fine

--- dadi/myscript.js
+++ dadi/myscript.js
@@ -1,17 +1,16 @@
 var numeroPc = Math.floor(Math.random()*6);
-console.log("Il numero generato dal compuer è " + numeroPc);
+console.log("Il numero generato dal computer è " + numeroPc);
 document.getElementById('iaNumber').innerHTML = ("Il numero generato dal computer è " + numeroPc);
 
 var numeroGiocatore = parseInt(prompt("Inserisci un numero tra 1 e 6"));
 console.log("Il tuo numero è " + numeroGiocatore);
-document.getElementById('humanNumber').innerHTML = ("Il numero generato dal computer è " + numeroGiocatore);
+document.getElementById('humanNumber').innerHTML = ("Il tuo numero è " + numeroGiocatore);
 
 
 if  ( numeroPc > numeroGiocatore) {
-    console.log("Pc vince")
-
-}
-
-if (numeroGiocatore > numeroPc) {
-    console.log("Giocatore umano vince")
+    console.log("Pc vince");
+} else if (numeroGiocatore > numeroPc) {
+    console.log("Giocatore umano vince");
+} else {
+    console.log ("Pareggio");
 }
